1a63ae74027e53ff979d6ad6aa97c48eaf493d76
Likhon-25 <likhonmondol327@gmail.com>
2026-02-19 15:14:18 +0600

    cashout condition added

--- script/cashout.js
+++ script/cashout.js
@@ -1,38 +1,72 @@
-console.log("withdraw done")
-document.getElementById('cashout-btn').addEventListener('click', function () {
-    // 1- get the agent number & validate
-    const cashoutNumberInput = document.getElementById('cashout-number');
-    const cahsoutNumber = cashoutNumberInput.value;
-    console.log('Agent Number =', cahsoutNumber)
-    if(cahsoutNumber.length !== 11){
-        alert('Invlid Agent Number')
-        return
-    }
-    // 2- get the amount , validate, convert to number
-    const cashoutAmountInput = document.getElementById('cashout-amount');
-    const cashoutAmount = cashoutAmountInput.value;
-    console.log('cashout Amount = ', cashoutAmount)
-    // 3- get the Current Balance, valdate, convert to number
-    const balanceElement = document.getElementById('balance')
-    const balance = balanceElement.innerText;
-    console.log('Balance = ', balance)
-    // 4- Calculate new Balance
-    const newBalance = Number(balance) - Number(cashoutAmount);
-    if (newBalance < 0) {
-        alert('Invalid Amount');
-        return
-    }
-    // 5- Get the pin and verify
-    const cashoutPinInput = document.getElementById('cashout-pin');
-    const pin = cashoutPinInput.value;
-    if (pin === '2524') {
-        // 5-1 true:: show an alert > set Balance
-        alert('Cashout Successfully')
-        console.log("New Balance = ", newBalance);
-        balanceElement.innerText = newBalance
-    }
-    else{
-        // 5-1 false:: show an error alert > return
-        alert('Invlaid Password')
-    }
-})
+document.getElementById('cashout-btn')
+    .addEventListener('click', function () {
+        // 1- get the agent number & validate
+        const cashoutNumber = getValueFromInput('cashout-number');
+        if (cashoutNumber.length !== 11) {
+            alert('Invlaid Number')
+            return
+        }
+
+        // 2- get the amount
+        const cashoutAmount = getValueFromInput('cashout-amount')
+
+        const currentBalance = getBalance()
+        // 4- calculate balance
+        const newBalance = currentBalance - Number(cashoutAmount);
+
+        if (newBalance < 0) {
+            alert('Invlaid Amount');
+            return;
+        }
+        const pin = getValueFromInput('cashout-pin');
+        if (pin === '2524') {
+            alert('cashout saccessfull');
+           setBalance(newBalance)
+        }
+        else {
+            alert('Invlaid pin');
+            return
+        }
+    })
+
+
+
+// ! 1st code
+// console.log("withdraw done")
+// document.getElementById('cashout-btn').addEventListener('click', function () {
+//     // 1- get the agent number & validate
+//     const cashoutNumberInput = document.getElementById('cashout-number');
+//     const cahsoutNumber = cashoutNumberInput.value;
+//     console.log('Agent Number =', cahsoutNumber)
+//     if(cahsoutNumber.length !== 11){
+//         alert('Invlid Agent Number')
+//         return
+//     }
+//     // 2- get the amount , validate, convert to number
+//     const cashoutAmountInput = document.getElementById('cashout-amount');
+//     const cashoutAmount = cashoutAmountInput.value;
+//     console.log('cashout Amount = ', cashoutAmount)
+//     // 3- get the Current Balance, valdate, convert to number
+//     const balanceElement = document.getElementById('balance')
+//     const balance = balanceElement.innerText;
+//     console.log('Balance = ', balance)
+//     // 4- Calculate new Balance
+//     const newBalance = Number(balance) - Number(cashoutAmount);
+//     if (newBalance < 0) {
+//         alert('Invalid Amount');
+//         return
+//     }
+//     // 5- Get the pin and verify
+//     const cashoutPinInput = document.getElementById('cashout-pin');
+//     const pin = cashoutPinInput.value;
+//     if (pin === '2524') {
+//         // 5-1 true:: show an alert > set Balance
+//         alert('Cashout Successfully')
+//         console.log("New Balance = ", newBalance);
+//         balanceElement.innerText = newBalance
+//     }
+//     else{
+//         // 5-1 false:: show an error alert > return
+//         alert('Invlaid Password')
+//     }
+// })
